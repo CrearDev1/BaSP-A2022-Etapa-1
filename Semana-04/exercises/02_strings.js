8c4b956b66e5fe3c18a81de36a3de5a5dfd15389
Santiago Torrens <stc.enprend@hotmail.com>
2022-09-17 12:06:27 -0300

    BaSP-W04-ex02-strings: Complete block

---
+++ Semana-04/exercises/02_strings.js
@@ -0,0 +1,18 @@
+console.log("EXERCISES-02-A");
+/**Crear una variable de tipo string con al menos 10 caracteres y
+ *  convertir todo el texto en mayúscula (utilizar toUpperCase). */
+var word="hello world ! , hello world!";
+console.log(word.toUpperCase());
+
+console.log("EXERCISES-02-B");
+/*Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los primeros 5 
+caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+subWord=word.substring(0,5);
+console.log(subWord);
+
+console.log("EXERCISES-02-C");
+/**Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con los últimos 
+ * 3 caracteres guardando el resultado en una nueva variable (utilizar substring). */
+console.log(word.length);
+subWord=word.substring(25,28);
+console.log(subWord);
