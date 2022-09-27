644dbb56ee6ddbb82309daa76953cbf4484b2753
CrearDev1 <stc.enprend@hotmail.com>
2022-09-27 10:59:12 -0300

    BaSP-W06-2022:Add p label bellow of inputs for message error, create validate functions,complete validate

--- Semana-05/javascipt/login.js
+++ Semana-05/javascipt/login.js
@@ -4,47 +4,89 @@
        
 window.onload= function(){
 
-    var campo=document.getElementById('email').addEventListener('blur',function(){
-        campo= event.target;
-        var emailOk=document.getElementById('valido1');
+
+    var letras="abcdefghyjklmnñopqrstuvwxyz";
+
+    function tiene_letras(texto){
+    for(i=0; i<texto.length; i++){
+       if (letras.indexOf(texto.charAt(i),0)!=-1){
+          return 1;
+       }
+    }
+    return 0;
+ }
+
+ var numeros="0123456789";
+
+ function tiene_numeros(texto){
+    for(i=0; i<texto.length; i++){
+       if (numeros.indexOf(texto.charAt(i),0)!=-1){
+          return 1;
+       }
+    }
+    return 0;
+ }
+    
+    var campoE=document.getElementById('email').addEventListener('blur',function (){
+        campoE= event.target;
         var emailRegex=/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/; 
-        if(emailRegex.test(campo.value)){
-            emailOk.innerText='Correcto';
+        if(emailRegex.test(campoE.value)){
+            campoE.style.border='solid 2px green';
         }else{
-            emailOk.innerText='Incorrecto Ej: email1@gmail.com';
+            campoE.style.border='solid 2px red';
+            var messageE=document.getElementsByClassName('p');
+            messageE[0].innerText='**Email error Ex:email1@gmail.com**'
+            messageE[0].style.marginTop='2%'
         }
     })    
-   var campo1=document.getElementById('email').addEventListener('focus', function(){
-        campo1= event.target;
-        var emailOk=document.getElementById('valido1');
+   var campoE=document.getElementById('email').addEventListener('focus', function(){
+        campoE= event.target;
         var emailRegex=/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/;
-        if(emailRegex.test(campo1.value)){
-            emailOk.innerText='';
+        if(emailRegex.test(campoE.value)){
+            campoE.style.border='none';
         }else{
-            emailOk.innerText='';
+            campoE.style.border='none';
+            var messageE=document.getElementsByClassName('p');
+            messageE[0].innerText=''
         }
     })         
 
-    var campo2=document.getElementById('password').addEventListener('blur',function(){
-        campo2= event.target;
-        var passwordOk=document.getElementById('valido2'); 
-        if(!isNaN(campo2.value)&&campo2.value.length>3){
-            passwordOk.innerText='Correcto';
+    var campoP=document.getElementById('password').addEventListener('blur',function (){
+        campoP= event.target; 
+        if(tiene_letras(campoP.value)!=0&&tiene_numeros(campoP.value)!=0&&(campoP.value.length)>=8){
+            campoP.style.border='solid 2px green';
         }else{
-            passwordOk.innerText='Incorrecto Ej: password1234';
+            campoP.style.border='solid 2px red';
+            var messageP=document.getElementsByClassName('p');
+            messageP[1].innerText='**Password error: use numbers and letters \nand more than 8 characters**';
+            messageP[1].style.marginTop='2%';
         }
-    }) 
-    var campo3=document.getElementById('password').addEventListener('focus', function(){
-        campo3= event.target;
-        var passwordOk=document.getElementById('valido2');
-        if(!isNaN(campo2.value)&&campo2.value.length>3){
-            passwordOk.innerText='';
+        
+}); 
+    var campoP=document.getElementById('password').addEventListener('focus', function(){
+        campoP= event.target;
+        if(tiene_letras(campoP.value)!=0&&tiene_numeros(campoP.value)!=0&&(campoP.value.length)>=8){
+            campoP.style.border='none';
         }else{
-            passwordOk.innerText='';
+            campoP.style.border='none';
+            var messageP=document.getElementsByClassName('p');
+            messageP[1].innerText=''
         }
     })         
 
-
+    var submit =document.getElementById('submit').addEventListener('click', function(e){
+        e.preventDefault();
+        var emailRegex=/^[^@]+@[^@]+\.[a-zA-Z]{2,}$/; 
+        var campoE=document.getElementById('email');
+        var campoP=document.getElementById('password');
+        if(emailRegex.test(campoE.value)&&tiene_letras(campoP.value)!=0&&tiene_numeros(campoP.value)!=0&&(campoP.value.length)>=8){
+            alert('Email: '+campoE.value+'\n'+'Password :'+campoP.value);
+        }else if(emailRegex.test(campoE.value)){
+            alert('Email: '+campoE.value+'\n'+'Password :'+campoP.value+'\n**Password error: use numbers and letters \nand more than 8 characters**');
+        }else if(tiene_letras(campoP.value)!=0&&tiene_numeros(campoP.value)!=0&&(campoP.value.length)>=8){
+            alert('Email: '+campoE.value+'\n'+'Password :'+campoP.value+'\n**Email error Ex:email1@gmail.com**');
+        }
+    })
 }
 
 
